b9d8ed1271103b6af12ace6cdf7b6e16c93f879e
Emmanuel Momoh <102439386+nuel000@users.noreply.github.com>
2024-04-14 04:03:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 def run():
     url = "https://web.facebook.com/ads/library/?id=357741606925594"
@@ -13,6 +14,7 @@ def run():
     response_text = r.text
 
     print(response_text)
+    sys.stdout.flush()
 
 if __name__ == "__main__":
     run()
