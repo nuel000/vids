aeec8d22b3a350a04d8568e7e7967fbaf9b6a756
Emmanuel Momoh <102439386+nuel000@users.noreply.github.com>
2025-01-21 11:09:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,45 @@
-import requests
 import sys
+from playwright.sync_api import Playwright, sync_playwright
+import time
+from bs4 import BeautifulSoup
 
-def run():
-    url = "https://web.facebook.com/ads/library/?id=357741606925594"
-    splash_url = "http://splash:8050/render.html"
-
-    params = {
-        'url': url,
-        'wait': 5
-    }
-
-    r = requests.get(splash_url, params=params)
-    response_text = r.text
-
-    print(response_text)
+def flushstd(message):
+    print(message)
     sys.stdout.flush()
 
-if __name__ == "__main__":
-    run()
+def run(playwright: Playwright) -> None:
+    browser = playwright.firefox.launch(headless=True)  # Use headless=True for CI environments
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://accounts.google.com")
+
+    time.sleep(3)
+    flushstd("Navigated to Google account login page.")
+    page.get_by_label("Email or phone").fill("momohemmanuel073")
+    flushstd("Email/phone field filled.")
+    time.sleep(3)
+    page.get_by_label("Email or phone").press("Enter")
+    flushstd("Pressed Enter for email/phone field.")
+    time.sleep(3)
+    page.get_by_label("Enter your password").fill("Ilovemymummy22@@..")
+    flushstd("Password field filled.")
+    time.sleep(3)
+    page.get_by_label("Enter your password").press("Enter")
+    flushstd("Pressed Enter for password.")
+    time.sleep(6)
+
+    page.goto("https://myadcenter.google.com/controls?ref=my-account&ref-media=WEB&hl=en")
+    flushstd("Navigated to My Ad Center page.")
+
+    time.sleep(3)
+    s = BeautifulSoup(page.content(), 'html.parser')
+    google_account_info = s.find('div', class_='KT87l').text if s.find('div', class_='KT87l') else "Info not found"
+    others = s.find('ul', class_='NBZP0e cIN7te xbmkib').text if s.find('ul', class_='NBZP0e cIN7te xbmkib') else "Others not found"
+    flushstd(f"Google Account Info: {google_account_info}")
+    flushstd(f"Others: {others}")
+    # ---------------------
+    context.close()
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
